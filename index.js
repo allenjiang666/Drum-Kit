e78e1fe518fa7efeffe91faabab566ca5bcb872b
Yipeng Jiang <60659607+jiangyipeng718@users.noreply.github.com>
2021-04-28 13:13:04 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,69 @@
+var buttons = document.querySelectorAll("button.drum");
+
+for (let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener("click", function (){
+        var buttonInnerHTML = this.innerHTML;
+        makeSound(buttonInnerHTML);
+        addAnimation(buttonInnerHTML);
+    });
+    
+}
+
+document.addEventListener("keypress",function (event) {
+    makeSound(event.key);
+    addAnimation(event.key)
+});
+
+function addAnimation(currentKey) {
+    var activeButton = document.querySelector('.'+ currentKey);
+    activeButton.classList.add('pressed');
+    setTimeout(function () {
+        activeButton.classList.remove('pressed');
+    },100);
+}
+
+function makeSound(key) {
+
+    switch (key) {
+        case "w":
+            var crash = new Audio('sounds/crash.mp3');
+            crash.play();
+            break;
+
+        case "a":
+            var kick = new Audio('sounds/kick-bass.mp3');
+            kick.play();
+            break;
+
+        case "s":
+            var snare = new Audio('sounds/snare.mp3');
+            snare.play();
+            break;
+
+        case "d":
+            var tom1 = new Audio('sounds/tom-1.mp3');
+            tom1.play();
+            break;
+
+        case "j":
+            var tom2 = new Audio('sounds/tom-2.mp3');
+            tom2.play();
+            break;
+
+        case "k":
+            var tom3 = new Audio('sounds/tom-3.mp3');
+            tom3.play();
+            break;
+
+        case "l":
+            var tom4 = new Audio('sounds/tom-4.mp3');
+            tom4.play();
+            break;
+
+    
+        default:
+            console.log(key)
+            break;
+    }
+}
+
